9dfc5eb9368aa72477b4857c0e5b5ad88ea6776d
Edwin Chen <edwinchen018@gmail.com>
2023-06-30 13:42:15 -0600

    Initial Commit

---
+++ TheVault/src/test/java/com/TheVault/TheVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TheVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
