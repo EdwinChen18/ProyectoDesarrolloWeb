95cb992f426c8ad06eed94667e720e6b99356666
Edwin Chen <edwinchen018@gmail.com>
2023-08-02 16:53:55 -0600

    correciones a parte de usuario + se agrego dependencias

--- TheVault/src/main/java/com/TheVault/Service/impl/UsuarioDetailsServiceImpl.java
+++ TheVault/src/main/java/com/TheVault/Service/impl/UsuarioDetailsServiceImpl.java
@@ -13,32 +13,37 @@ import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @Service("userDetailsService")
 public class UsuarioDetailsServiceImpl implements UsuarioDetailsService, UserDetailsService {
-    
+
     @Autowired
     private UsuarioDao usuarioDao;
     @Autowired
     private HttpSession session;
-    
+
     @Override
-    @Transactionl(readOnly = true)
+    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Usuario usuario = usuarioDao.findByUsername(username);
-        
-        if(usuario== null){
-            throw new UsernameNotFoundExption(msg:username);
-    }
-    
-    session.removeAtribute(string:"usuarioImagen");
-    session.setAttribute(string:"usuarioImagen", o:Usuario.getRutaImagen());
-    
-    var roles = ArrayList<GrantedAuthority>();
-    for (Rol rol : usuario.getRoles()){
-        roles.add(new SimpleGrantedAuthority(role:rol.getNombre()));
-    }
-    
-    return new User(username:usuario.getUsername(), password:usuario.getPassword(), authorities:roles);
+
+        if (usuario == null) {
+
+            throw new UsernameNotFoundException(username);
+
+        }
+
+        session.removeAttribute("usuarioImagen");
+        session.setAttribute("usuarioImagen", usuario.getRutaImagen());
+        var roles = new ArrayList<GrantedAuthority>();
+
+        for (Rol rol : usuario.getRoles()) {   //Se sacan los roles
+
+            roles.add(new SimpleGrantedAuthority(rol.getNombre()));
+
+        }
+
+        //Se devuelve User (clase de userDetails)
+        return new User(usuario.getUsername(), usuario.getPassword(), roles);
+
     }
 }
